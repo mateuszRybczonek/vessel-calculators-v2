dc7151668a2a2b77ff8f67612e64df95b4494efe
Mateusz Rybczonek <m.rybczonek@gmail.com>
2016-04-28 00:01:23 +0200

    - creating WPT objects and adding to WPT table;

--- src/app/rigMoveCalculator/rigMoveCalculator.js
+++ src/app/rigMoveCalculator/rigMoveCalculator.js
@@ -20,12 +20,22 @@ function rigMoveController($scope) {
     'Time leg',
     'Time remaining',
     'Selected'];
+  $scope.WPTS = [];
 
   $scope.WPT = WPT;
+  $scope.addNewWPT = addNewWPT;
+
+  function WPT(name, easting, northing, index) {
+    this.name = name;
+    this.easting = easting;
+    this.northing = northing;
+    this.index = index;
+  }
+
+  function addNewWPT (name, easting, northing){
+    var newWPT = new WPT(name, easting, northing, $scope.WPTS.length+1);
+    $scope.WPTS.push(newWPT);
+    console.log($scope.WPTS);
+  }
 }
 
-function WPT(name, easting, northing) {
-  this.name = name;
-  this.easting = easting;
-  this.northing = northing;
-}
