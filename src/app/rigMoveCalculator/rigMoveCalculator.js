104fabeffa9f6daec5857ac6c60cc2602af53cfb
Mateusz Rybczonek <m.rybczonek@gmail.com>
2016-05-03 15:51:21 +0200

    - minor changes;

--- src/app/rigMoveCalculator/rigMoveCalculator.js
+++ src/app/rigMoveCalculator/rigMoveCalculator.js
@@ -34,6 +34,7 @@ function rigMoveController($scope) {
   $scope.saveChanges = saveChanges;
   $scope.moveUp = moveUp;
   $scope.moveDown = moveDown;
+  $scope.calculate = calculate;
 
   function WPT(name, easting, northing, index) {
     this.name = name;
@@ -45,6 +46,7 @@ function rigMoveController($scope) {
   function addNewWPT() {
     var newWPT = new WPT("", "", "", "");
     $scope.WPTS.push(newWPT);
+    calculate();
   }
 
   function exportToJSON() {
@@ -99,15 +101,16 @@ function rigMoveController($scope) {
 
   function saveChanges(index, name, easting, northing) {
     $scope.WPTS[index] = {'index': index + 1, 'name': name, 'easting': easting, 'northing': northing};
+    calculate();
   }
 
   function removeRow(selectedIndex) {
     $scope.WPTS.splice(selectedIndex, 1);
+    debugger;
     $scope.WPTS.map(function (WPT, index) {
       WPT.index = index + 1;
     });
-    console.log($scope.WPTS);
-
+    calculate();
   }
 
   function moveRow(start, end) {
@@ -120,12 +123,96 @@ function rigMoveController($scope) {
 
   function moveUp(index) {
     moveRow(index, index - 1);
+    calculate();
   }
 
   function moveDown(index) {
     moveRow(index, index + 1);
+    calculate();
   }
 
+  function calculate() {
+    var iteration;
+    var myTable = $('.tableWPT>tbody')[0]; //select table as var
+    var rowCount = myTable.rows.length;
+    var eastingDifference, northingDifference, dDistanceLeg, dDistanceDone, dDD1, dDD2, dTimeLeg, speed, hours, minutes, dDistanceRemaining, dDistanceRemaining1stRow, dTimeRemaining, dTimeRemaining1stRow, speed1stRow, hours1stRow, minutes1stRow;
+
+    function legDistanceCalculation() {
+      for (iteration = 1; iteration < rowCount; iteration++) {
+        eastingDifference = (myTable.rows[iteration].cells[3].textContent - myTable.rows[iteration - 1].cells[3].textContent);
+        northingDifference = (myTable.rows[iteration].cells[5].textContent - myTable.rows[iteration - 1].cells[5].textContent);
+        dDistanceLeg = Math.sqrt(eastingDifference * eastingDifference + northingDifference * northingDifference);
+        myTable.rows[iteration].cells[7].textContent = dDistanceLeg.toFixed(0);
+      }
+    }
+
+    function distanceDoneCalculation() {
+      for (iteration = 1; iteration < rowCount; iteration++) {
+        //if previous distanceDone empty
+        if (myTable.rows[iteration - 1].cells[7].textContent == null) {
+          dDistanceDone = parseFloat(0 + myTable.rows[iteration].cells[7].textContent);
+        }
+        else if (myTable.rows[iteration - 1].cells[7].textContent == "") {
+          dDistanceDone = parseFloat(0 + myTable.rows[iteration].cells[7].textContent);
+        }
+        else {
+          dDD1 = myTable.rows[iteration - 1].cells[8].textContent; //previous leg
+          dDD2 = myTable.rows[iteration].cells[7].textContent; // current leg
+          dDistanceDone = parseFloat(dDD1) + parseFloat(dDD2);
+        }
+
+        myTable.rows[iteration].cells[8].textContent = dDistanceDone;
+      }
+    }
+
+    function legTimeCalculation() {
+      for (iteration = 1; iteration < rowCount; iteration++) {
+        {
+          speed = $('.projected-speed-input').val();
+          dTimeLeg = (myTable.rows[iteration].cells[7].textContent) / 1852 / speed;
+          hours = Math.round(dTimeLeg);
+          minutes = ((dTimeLeg - hours) * 60);
+          if (minutes < 0) minutes = 60 + minutes;
+          myTable.rows[iteration].cells[10].textContent = hours + " h " + minutes.toFixed(0) + " min";
+        }
+      }
+    }
+
+    function remainingTimeCalculation() {
+      for (iteration = 1; iteration < rowCount; iteration++) {
+        dDistanceRemaining = myTable.rows[rowCount - 1].cells[8].textContent
+          - myTable.rows[iteration].cells[8].textContent;
+        myTable.rows[iteration].cells[9].textContent = dDistanceRemaining;
+
+        speed = $('.projected-speed-input').val();
+        dTimeRemaining = dDistanceRemaining / 1852 / speed;
+        hours = Math.round(dTimeRemaining);
+        minutes = ((dTimeRemaining - hours) * 60);
+        if (minutes < 0) minutes = 60 + minutes;
+        myTable.rows[iteration].cells[11].textContent = hours + " h " + minutes.toFixed(0) + " min";
+      }
+    }
+
+    function distanceRemainingAndTimeRemaining1stRow() {
+      dDistanceRemaining1stRow = myTable.rows[rowCount - 1].cells[8].textContent;
+      myTable.rows[0].cells[9].textContent = dDistanceRemaining1stRow;
+      speed1stRow = $('.projected-speed-input').val();
+
+      dTimeRemaining1stRow = dDistanceRemaining1stRow / 1852 / speed1stRow;
+      hours1stRow = Math.round(dTimeRemaining1stRow);
+      minutes1stRow = (dTimeRemaining1stRow - hours1stRow) * 60;
+      if (minutes1stRow < 0) minutes1stRow = 60 + minutes1stRow;
+      myTable.rows[0].cells[11].textContent = hours1stRow + " h " + minutes1stRow.toFixed(0) + " min";
+    }
+
+
+    legDistanceCalculation();
+    distanceDoneCalculation();
+    legTimeCalculation();
+    remainingTimeCalculation();
+    distanceRemainingAndTimeRemaining1stRow();
+    //drawing();
+  }
 }
 
 
